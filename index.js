ab4c05744b353c4a83f0a1d5efb2e267639a8a27
Max Sychev <sychevmax@gmail.com>
2022-11-13 01:21:26 +0400

    add express web server

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const { Telegraf } = require('telegraf');
 const dotenv = require('dotenv');
+const express = require('express');
+const packageInfo = require('package.json');
 
 dotenv.config();
 
@@ -14,3 +16,16 @@ bot.on("message", (ctx) => {
 })
 
 bot.launch();
+
+var app = express();
+
+app.get('/', function(req, res) {
+    res.json({ version: packageInfo.version });
+});
+
+var server = app.listen(process.env.PORT, function() {
+    var host = server.address().address;
+    var port = server.address().port;
+
+    console.log('Web server started at http://%s:%s', host, port);
+});
