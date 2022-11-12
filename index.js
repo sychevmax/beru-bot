58b33bb456169aaf5877f79485d3ef48379a4df4
Max Sychev <sychevmax@gmail.com>
2022-11-13 01:03:52 +0400

    simple bot to test deploy

---
+++ index.js
@@ -0,0 +1,16 @@
+const { Telegraf } = require('telegraf');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const bot = new Telegraf(process.env.botToken);
+
+bot.start((ctx) => {
+  ctx.reply("Привет, красавчик! Один здесь?");
+});
+
+bot.on("message", (ctx) => {
+  ctx.reply("Ок");
+})
+
+bot.launch();
